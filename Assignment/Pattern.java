563bee8fe0545968ad94972d6f36f7de40fd7f84
Sonu <sonuby93@gmail.com>
2022-06-01 20:58:37 +0530

    All programs till 01-06

--- Assignment/Pattern.java
+++ Assignment/Pattern.java
@@ -1,11 +1,11 @@
-package Java_Basic;
+package Assignment;
 
-public class Pattern1 {
+public class Pattern {
 	
 	public static void main(String[] args) {
 		for(int i=0;i<5;i++) {
-			for(int j=0;j<=i;j++) {
-				System.out.print("* ");
+			for(int j=0;j<5-i;j++) {
+				System.out.print('*');
 			}
 			
 			System.out.println();
